75eb15bb8fd0a9071bc03922f3197fb7d099f2ce
GisellyReis <gisellyareis@gmail.com>
2022-12-12 17:40:57 -0300

    1º project - I'm Here

---
+++ imhere/src/components/Participant/index.tsx
@@ -0,0 +1,26 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+}
+
+export function Participant({ name, onRemove }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        { name }
+      </Text>
+
+      <TouchableOpacity 
+          style={styles.btn}
+          onPress={onRemove}
+        >
+          <Text style={styles.btnText}>
+            -
+          </Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
