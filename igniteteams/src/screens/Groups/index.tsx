b9ace0b3939a8774cf791d39e829145d96b0f193
GisellyReis <gisellyareis@gmail.com>
2022-12-22 15:12:24 -0300

    Componente Highlight

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight.tsx';
 import { 
   Container 
 } from './styles'
@@ -8,6 +9,10 @@ export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title='Turmas' 
+        subtitle='jogue com a sua turma'
+      />
     </Container>
   );
 }
