85fc9438b002d0a262f34918e067b9cbf18ed459
GisellyReis <gisellyareis@gmail.com>
2022-12-23 23:56:47 -0300

    Componente List Empty

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight.tsx';
 import { 
   Container 
 } from './styles'
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -14,8 +18,19 @@ export function Groups() {
         title='Turmas' 
         subtitle='jogue com a sua turma'
       />
-
-      <GroupCard title='Galera do Ignite' />
+      
+      <FlatList 
+        data={groups}
+        keyExtractor={ item => item }
+        renderItem={({ item }) => (
+          <GroupCard title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        )}
+      />
     </Container>
   );
 }
