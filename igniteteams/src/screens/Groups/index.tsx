a1c387626409bccb9257fc6b5820b12832c77162
GisellyReis <gisellyareis@gmail.com>
2022-12-24 00:12:14 -0300

    Componente Button

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -7,6 +7,7 @@ import {
 } from './styles'
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -30,6 +31,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message='Que tal cadastrar a primeira turma?' />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
