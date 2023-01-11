1880595fc5adb75cdb3db882204b9b7bc3e22b4c
GisellyReis <gisellyareis@gmail.com>
2023-01-11 18:27:30 -0300

    Componente de Input

--- ignitegym/App.tsx
+++ ignitegym/App.tsx
@@ -1,19 +1,22 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded]  = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{
-      flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#202024' 
-    }}>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Text>Hello React Native</Text> : <View />}
-    </View>
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
---
+++ ignitegym/src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner, Center } from 'native-base'
+export function Loading() {
+  return (
+    <Center flex={1}>
+      <Spinner />
+    </Center>
+  )
+}
