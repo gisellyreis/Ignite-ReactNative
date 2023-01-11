e07036ec71c1e613a3360bdfb35ee06212d09ba9
GisellyReis <gisellyareis@gmail.com>
2023-01-11 18:47:44 -0300

    Componente de Button

--- ignitegym/src/screens/SignIn.tsx
+++ ignitegym/src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 
 export function SignIn() {
   return (
@@ -35,7 +36,11 @@ export function SignIn() {
           placeholder='Senha' 
           secureTextEntry
         />
+
+        <Button title='Acessar' />
       </Center>
+
+      <Button title='Criar conta' variant="outline" />
     </VStack>
   )
 }
