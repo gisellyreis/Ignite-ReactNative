a5a5d10e188d42ac85d9e489f426ec0c444cec53
gisellyreis <gisellyareis@gmail.com>
2023-01-13 10:41:35 -0300

    Rotas públicas - navegação de autenticação

--- ignitegym/src/screens/SignIn.tsx
+++ ignitegym/src/screens/SignIn.tsx
@@ -1,19 +1,29 @@
+import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 
 export function SignIn() {
+
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView 
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+      <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -49,7 +59,11 @@ export function SignIn() {
             Ainda não tem acesso?
           </Text>
 
-          <Button title='Criar conta' variant="outline" />
+          <Button 
+            title='Criar conta' 
+            variant="outline" 
+            onPress={handleNewAccount}
+          />
         </Center>
       </VStack>
     </ScrollView>
