230703a25ae0f76571da5a76b24e7846bc333b9a
gisellyreis <gisellyareis@gmail.com>
2023-01-18 14:48:12 -0300

    Home - listagem de grupos

--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,10 +1,32 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
+    const [groupSelected, setGroupSelected] = useState('')
+
     return (
         <VStack flex={1}>
             <HomeHeader />
+
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group 
+                        name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
         </VStack>
     )
 }
