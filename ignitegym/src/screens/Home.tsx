b38aaa35bd2f99d927ed403d0347b8cf714a082c
gisellyreis <gisellyareis@gmail.com>
2023-01-18 15:30:01 -0300

    Lista de exercícios

--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { Center, FlatList, Heading, HStack, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function Home() {
@@ -27,6 +27,20 @@ export function Home() {
                 my={10}
                 maxH={10}
             />
+
+            <VStack px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+
+                    <Text color="gray.200" fontSize="md">
+                        4
+                    </Text>
+                </HStack>
+
+                
+            </VStack>
         </VStack>
     )
 }
