580ad19f51739a285cd6e7334491ba68d5b70783
gisellyreis <gisellyareis@gmail.com>
2023-01-19 11:56:44 -0300

    Interface de Perfil - Foto do usuário

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -1,9 +1,38 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
     return (
-        <Center flex={1}>
-            <Text>Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil" />
+
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ? 
+                        <Skeleton w={PHOTO_SIZE} h={PHOTO_SIZE} rounded="full" startColor="gray.500" endColor="gray.400"/>
+                        :
+                        <UserPhoto
+                            source={{ uri: 'https://github.com/gisellyreis.png' }}
+                            alt="Foto do usuário"
+                            size={PHOTO_SIZE}
+                        />
+                    }
+
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar Foto
+                        </Text>
+                    </TouchableOpacity>
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }
