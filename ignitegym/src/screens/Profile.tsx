bbb3e16a20b4f21bf59271e2e5c6a08413136a1b
gisellyreis <gisellyareis@gmail.com>
2023-01-19 12:39:03 -0300

    Interface Profile

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
@@ -13,7 +15,7 @@ export function Profile() {
         <VStack flex={1}>
             <ScreenHeader title="Perfil" />
 
-            <ScrollView>
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
                 <Center mt={6} px={10}>
                     {
                         photoIsLoading ? 
@@ -31,7 +33,21 @@ export function Profile() {
                             Alterar Foto
                         </Text>
                     </TouchableOpacity>
+
+                    <Input bg="gray.600" placeholder="Nome" />
+                    <Input bg="gray.600" placeholder="E-mail" isDisabled />
+
+                    <Heading color="gray.200" fontSize="md" mb={2} mt={12} alignSelf="flex-start">
+                        Alterar senha
+                    </Heading>
+
+                    <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+                    <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+                    <Input bg="gray.600" placeholder="Confirme a nova senha" secureTextEntry />
+
+                    <Button title="Atualizar" mt={4} />
                 </Center>
+
             </ScrollView>
         </VStack>
     )
