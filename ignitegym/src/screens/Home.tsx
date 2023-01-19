1b0c1145e0a53bfd562d0dfc7b5abb69689f1a25
gisellyreis <gisellyareis@gmail.com>
2023-01-19 13:24:25 -0300

    Interface Exercise

--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,13 +1,21 @@
 import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { Center, FlatList, Heading, HStack, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function Home() {
     const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
     const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento'])
-    const [groupSelected, setGroupSelected] = useState('')
+    const [groupSelected, setGroupSelected] = useState('Costas')
+
+    const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+    function handleOpenExerciseDetails() {
+        navigation.navigate('exercise')
+    }
 
     return (
         <VStack flex={1}>
@@ -45,7 +53,7 @@ export function Home() {
                     data={exercises}
                     keyExtractor={item => item}
                     renderItem={({ item }) => (
-                        <ExerciseCard />
+                        <ExerciseCard onPress={handleOpenExerciseDetails} />
                     )}
                     showsVerticalScrollIndicator={false}
                     _contentContainerStyle={{
