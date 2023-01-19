48415c8219637243be972bf06f29b8e7ceef14c8
gisellyreis <gisellyareis@gmail.com>
2023-01-19 10:18:25 -0300

    Componente HistoryCard

---
+++ ignitegym/src/components/HistoryCard.tsx
@@ -0,0 +1,23 @@
+import { Heading, HStack, VStack , Text} from "native-base";
+
+export function HistoryCard() {
+    return (
+        <HStack 
+            w="full" px={5} py={4} mb={3} bg="gray.600" rounded="md" alignItems="center" justifyContent="space-between"
+        >
+            <VStack mr={5}>
+                <Heading color="white" fontSize="md" textTransform="capitalize">
+                    Costas
+                </Heading>
+
+                <Text fontSize="lg" color="gray.100" mt={1} numberOfLines={1}>
+                   Puxada frontal
+                </Text>
+            </VStack>
+
+            <Text color="gray.300" fontSize="md">
+                08:56
+            </Text>
+        </HStack>
+    )
+}
