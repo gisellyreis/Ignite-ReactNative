2ea5343ca6ea8c76dc3752e66786672db9a08fea
GisellyReis <gisellyareis@gmail.com>
2023-01-20 22:45:37 -0300

    Galeria de fotos - Selecionando foto do perfil

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -2,19 +2,54 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, useToast, VStack } from "native-base";
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false)
+    const [userPhoto, setUserPhoto] = useState('https://img2.gratispng.com/20180402/ojw/kisspng-united-states-avatar-organization-information-user-avatar-5ac20804a62b58.8673620215226654766806.jpg')
+
+    const toast = useToast()
 
     async function handleUserPhotoSelect() {
-        await ImagePicker.launchImageLibraryAsync();
+        setPhotoIsLoading(true)
+
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true
+            });
+            
+            if(photoSelected.canceled) {
+                return
+            }
+
+            if(photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+                if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+                    return toast.show({
+                        title: 'Essa image é muito grande. Escolha uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    })
+                }
+
+                setUserPhoto(photoSelected.assets[0].uri)
+            }
+    
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setPhotoIsLoading(false)
+        }
     }
 
     return (
@@ -28,7 +63,7 @@ export function Profile() {
                         <Skeleton w={PHOTO_SIZE} h={PHOTO_SIZE} rounded="full" startColor="gray.500" endColor="gray.400"/>
                         :
                         <UserPhoto
-                            source={{ uri: 'https://github.com/gisellyreis.png' }}
+                            source={{ uri: userPhoto }}
                             alt="Foto do usuário"
                             size={PHOTO_SIZE}
                         />
