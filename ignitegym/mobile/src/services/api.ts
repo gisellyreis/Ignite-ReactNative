7c1641e5b96b5edb870a72e75f4032dd17fa8ece
GisellyReis <gisellyareis@gmail.com>
2023-01-25 18:04:02 -0300

    Interceptors

--- ignitegym/mobile/src/services/api.ts
+++ ignitegym/mobile/src/services/api.ts
@@ -4,6 +4,13 @@ const api = axios.create({
   baseURL: 'http://192.168.1.32:3333'
 })
 
+api.interceptors.request.use((config) => {
+  console.log(config);
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
 api.interceptors.response.use((response) => {
   console.log("INTERCEPTOR =>", response)
   return response
