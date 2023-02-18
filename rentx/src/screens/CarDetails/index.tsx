ee43731bd836110678cb8ca57c982e7baad61d3b
GisellyReis <gisellyareis@gmail.com>
2023-02-18 12:23:49 -0300

    Car Details Screen

---
+++ rentx/src/screens/CarDetails/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Accessory } from '../../components/Accessory';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+import PeopleSvg from '../../assets/people.svg'
+
+import { 
+  Container, 
+  Header, 
+  CarImages, 
+  Content, 
+  Details, 
+  Description, 
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider imageUrl={['https://e7.pngegg.com/pngimages/183/675/png-clipart-audi-audi.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name='380Km/h' icon={SpeedSvg} />
+          <Accessory name='3.2s' icon={AccelerationSvg} />
+          <Accessory name='800 HP' icon={ForceSvg} />
+          <Accessory name='Gasolina' icon={GasolineSvg} />
+          <Accessory name='Auto' icon={ExchangeSvg} />
+          <Accessory name='2 pessoas' icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident eos illum totam? Autem animi, expedita, ex nulla sapiente, facilis eligendi at aliquam tenetur voluptatum accusantium non eveniet consequuntur culpa minima?
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title='Confirmar' />
+      </Footer>
+    </Container>
+  );
+}
